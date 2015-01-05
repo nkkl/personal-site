604fa3f444fa4a3c3c8ef675d889fee9fc0a68ac
Nikki Lee <nikki@nicoleblee.com>
2015-01-04 16:29:14 -0800

    added histogram data, cleaned up presentation

--- giving/survey.js
+++ giving/survey.js
@@ -1,3 +1,7 @@
+var irlColor = 'rgba(0,0,255,.6)';
+var internetColor = 'rgba(0,150,255,.6)';
+var strangerColor = 'rgba(0,255,255,.6)';
+
 var donorList = [];
 
 var irlDonors = [];
@@ -7,6 +11,18 @@ var strangerDonors = [];
 var irlPercentages = [];
 var internetPercentages = [];
 var strangerPercentages = [];
+
+var irlIncomeDistribution = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0]; // $0-400k range, by $25000s
+var internetIncomeDistribution = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0];
+var strangerIncomeDistribution = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0];
+
+var irlDonationDistribution = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0]; // $0-40k range, by $2500s
+var internetDonationDistribution = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0];
+var strangerDonationDistribution = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0];
+
+var irlPercentageDistribution = [0,0,0,0,0,0,0,0,0,0]; // 0-50% range, by 5s
+var internetPercentageDistribution = [0,0,0,0,0,0,0,0,0,0];
+var strangerPercentageDistribution = [0,0,0,0,0,0,0,0,0,0];
 
 // grab the data
 $.getJSON("https://spreadsheets.google.com/feeds/list/10yl4cQxdHsUh-D9RO8-Brc-YXFDeYJlHq1bvYJJOg3o/od6/public/values?alt=json-in-script&callback=?",
@@ -21,22 +37,46 @@ $.getJSON("https://spreadsheets.google.com/feeds/list/10yl4cQxdHsUh-D9RO8-Brc-YX
 					friendship: entry["gsx$reallifefriends"].$t
 				};
 
+				// percentage of income donated
+				var percentage = Math.round(10000*parseInt(donor.donation)/parseInt(donor.income))/100;
+
 				donorList.push(donor);
 
+				// capture relationship
 				switch (donor.friendship) {
 					case 'real life friends':
 						irlDonors.push([parseInt(donor.income), parseInt(donor.donation)]);
-						irlPercentages.push([parseInt(donor.income), Math.round(10000*parseInt(donor.donation)/parseInt(donor.income))/100]);
+						irlPercentages.push([parseInt(donor.income), percentage]);
+						// capture income histogram
+						irlIncomeDistribution[Math.ceil(donor.income/25000) - 1]++;
+						// capture donation histogram
+						irlDonationDistribution[Math.ceil(donor.donation/2500) - 1]++;
+						// capture percentage histogram
+						irlPercentageDistribution[Math.ceil(percentage/5) - 1]++;
 						break;
 					case 'internet friends':
 						internetDonors.push([parseInt(donor.income), parseInt(donor.donation)]);
-						internetPercentages.push([parseInt(donor.income), Math.round(10000*parseInt(donor.donation)/parseInt(donor.income))/100]);
+						internetPercentages.push([parseInt(donor.income), percentage]);
+						// capture income histogram
+						internetIncomeDistribution[Math.ceil(donor.income/25000) - 1]++;
+						// capture donation histogram
+						internetDonationDistribution[Math.ceil(donor.donation/2500) - 1]++;
+						// capture percentage histogram
+						internetPercentageDistribution[Math.ceil(percentage/5) - 1]++;
 						break;
 					case 'strangers':
 						strangerDonors.push([parseInt(donor.income), parseInt(donor.donation)]);
-						strangerPercentages.push([parseInt(donor.income), Math.round(10000*parseInt(donor.donation)/parseInt(donor.income))/100]);
+						strangerPercentages.push([parseInt(donor.income), percentage]);
+						// capture income histogram
+						strangerIncomeDistribution[Math.ceil(donor.income/25000) - 1]++;
+						// capture donation histogram
+						strangerDonationDistribution[Math.ceil(donor.donation/2500) - 1]++;
+						// capture percentage histogram
+						strangerPercentageDistribution[Math.ceil(percentage/5) - 1]++;
 						break;
 				}
+
+				
 			}
 
 	graphData();
@@ -107,7 +147,7 @@ var graphData = function() {
         },
         series: [{
         	name: 'real life friends',
-        	color: 'rgba(255,0,0,.5)',
+        	color: irlColor,
         	data: irlDonors,
         	marker: {
         		symbol: 'circle'
@@ -115,7 +155,7 @@ var graphData = function() {
         },
         {
         	name: 'internet friends',
-        	color: 'rgba(150,0,255,.5)',
+        	color: internetColor,
         	data: internetDonors,
         	marker: {
         		symbol: 'circle'
@@ -123,7 +163,7 @@ var graphData = function() {
         },
         {
         	name: 'strangers',
-        	color: 'rgba(0,0,255,.5)',
+        	color: strangerColor,
         	data: strangerDonors,
         	marker: {
         		symbol: 'circle'
@@ -138,7 +178,7 @@ var graphData = function() {
 			zoomType: 'xy'
 		},
 		title: {
-			text: 'Percentage of Income Donated'
+			text: 'Percentage of Income Donated by Income'
 		},
 		subtitle: {
 			text: 'Source: <a href="http://goo.gl/forms/PVi3bJ2Ii6">personal survey</a>'
@@ -196,7 +236,7 @@ var graphData = function() {
         },
         series: [{
         	name: 'real life friends',
-        	color: 'rgba(255,0,0,.5)',
+        	color: irlColor,
         	data: irlPercentages,
         	marker: {
         		symbol: 'circle'
@@ -204,7 +244,7 @@ var graphData = function() {
         },
         {
         	name: 'internet friends',
-        	color: 'rgba(150,0,255,.5)',
+        	color: internetColor,
         	data: internetPercentages,
         	marker: {
         		symbol: 'circle'
@@ -212,11 +252,239 @@ var graphData = function() {
         },
         {
         	name: 'strangers',
-        	color: 'rgba(0,0,255,.5)',
+        	color: strangerColor,
         	data: strangerPercentages,
         	marker: {
         		symbol: 'circle'
         	}
         }]
 	});
+
+	// income histogram
+    $('#incomehist').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Income (USD)'
+        },
+        xAxis: {
+            categories: ['$0-25k', '$25-50k', '$50-75k', '$75-100k', '$100-125k', '$125-150k', '$150-175k', '$175-200k', '$200-225k', '$225-250k', '$250-275k', '$275-300k', '$300-325k', '$325-350k', '$350-375k', '$375-400k']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'People'
+            },
+            stackLabels: {
+                enabled: true,
+                style: {
+                    fontWeight: 'bold',
+                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                }
+            }
+        },
+        legend: {
+            align: 'right',
+            x: -30,
+            verticalAlign: 'top',
+            y: 25,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            shadow: false
+        },
+        tooltip: {
+            formatter: function () {
+                return '<b>' + this.x + '</b><br/>' +
+                    this.series.name + ': ' + this.y + '<br/>' +
+                    'Total: ' + this.point.stackTotal;
+            }
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0,
+		        borderWidth: 0,
+		        groupPadding: 0,
+		        shadow: false,
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: false,
+                    color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                    style: {
+                        textShadow: '0 0 3px black'
+                    },
+                }
+            }
+        },
+        series: [
+        {
+            name: 'strangers',
+            color: strangerColor,
+            data: strangerIncomeDistribution
+        },
+        {
+            name: 'internet friends',
+            color: internetColor,
+            data: internetIncomeDistribution
+        },
+ 		{
+            name: 'real life friends',
+            color: irlColor,
+            data: irlIncomeDistribution
+        }]
+    });
+
+	// donation histogram
+    $('#donationhist').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Money Donated (USD)'
+        },
+        xAxis: {
+            categories: ['$0-2.5k', '$2.5-5k', '$5-7.5k', '$7.5-10k', '$10-12.5k', '$12.5-15k', '$15-17.5k', '$17.5-20k', '$20-22.5k', '$22.5-25k', '$25-27.5k', '$27.5-30k', '$30-32.5k', '$32.5-35k', '$35-37.5k', '$37.5-40k']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'People'
+            },
+            stackLabels: {
+                enabled: true,
+                style: {
+                    fontWeight: 'bold',
+                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                }
+            }
+        },
+        legend: {
+            align: 'right',
+            x: -30,
+            verticalAlign: 'top',
+            y: 25,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            shadow: false
+        },
+        tooltip: {
+            formatter: function () {
+                return '<b>' + this.x + '</b><br/>' +
+                    this.series.name + ': ' + this.y + '<br/>' +
+                    'Total: ' + this.point.stackTotal;
+            }
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0,
+		        borderWidth: 0,
+		        groupPadding: 0,
+		        shadow: false,
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: false,
+                    color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                    style: {
+                        textShadow: '0 0 3px black'
+                    },
+                }
+            }
+        },
+        series: [
+        {
+            name: 'strangers',
+            color: strangerColor,
+            data: strangerDonationDistribution
+        },
+        {
+            name: 'internet friends',
+            color: internetColor,
+            data: internetDonationDistribution
+        },
+ 		{
+            name: 'real life friends',
+            color: irlColor,
+            data: irlDonationDistribution
+        }]
+    });
+
+	// percentage donated histogram
+    $('#percentagehist').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Percentage of Income Donated'
+        },
+        xAxis: {
+            categories: ['0-5%', '5-10%', '10-15%', '15-20%', '20-25%', '25-30%', '30-35%', '35-40%', '40-45%', '45-50%']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'People'
+            },
+            stackLabels: {
+                enabled: true,
+                style: {
+                    fontWeight: 'bold',
+                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                }
+            }
+        },
+        legend: {
+            align: 'right',
+            x: -30,
+            verticalAlign: 'top',
+            y: 25,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            shadow: false
+        },
+        tooltip: {
+            formatter: function () {
+                return '<b>' + this.x + '</b><br/>' +
+                    this.series.name + ': ' + this.y + '<br/>' +
+                    'Total: ' + this.point.stackTotal;
+            }
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0,
+		        borderWidth: 0,
+		        groupPadding: 0,
+		        shadow: false,
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: false,
+                    color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                    style: {
+                        textShadow: '0 0 3px black'
+                    },
+                }
+            }
+        },
+        series: [
+        {
+            name: 'strangers',
+            color: strangerColor,
+            data: strangerPercentageDistribution
+        },
+        {
+            name: 'internet friends',
+            color: internetColor,
+            data: internetPercentageDistribution
+        },
+ 		{
+            name: 'real life friends',
+            color: irlColor,
+            data: irlPercentageDistribution
+        }]
+    });
 }
