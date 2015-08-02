ae6a804a98106c4a8b894d2c732a1d4f6bf72b1d
Nikki Lee <nikki@nicoleblee.com>
2015-08-02 10:21:22 -0700

    Updates to 6 months of giving

--- giving/six.js
+++ giving/six.js
@@ -1,36 +1,47 @@
 var donations = [
 	[
 		['PAWS Chicago', 'http://www.pawschicago.org/how-to-help/donate/'],
-		['Olin College Alumni Merit Scholarship Fund','https://fundit.olin.edu/project/555a1d3109206579b4677801']
+		['Olin College Alumni Merit Scholarship Fund','https://fundit.olin.edu/project/555a1d3109206579b4677801'],
+		['Students Rising Above', 'http://studentsrisingabove.org/']
 	],
 	[
 		['Slow Roll Chicago', 'http://www.slowrollchicago.org/donate/'],
-		['MarketShare', 'http://buildthemarket.org/donate/']
+		['MarketShare', 'http://buildthemarket.org/donate/'],
+		['The Cara Program', 'https://cara.secure.force.com/donate']
 	],
 	[
 		['Doctors Without Borders', 'http://www.doctorswithoutborders.org/'],
-		['Central City Hospitality House', 'http://hospitalityhouse.org/']
+		['Central City Hospitality House', 'http://hospitalityhouse.org/'],
+		['CHiPS', 'http://chipsonline.org/']
 	],
 	[
-		['Revolutions Classroom Research Library', 'http://www.donorschoose.org/project/revolutions-classroom-research-library/1565268']
+		['Revolutions Classroom Research Library', 'http://www.donorschoose.org/project/revolutions-classroom-research-library/1565268'],
+		['Coping with Mental Illness and Disability', 'http://www.donorschoose.org/project/coping-with-mental-illness-and-disabilit/1577174/'],
+		['Living Goods', 'http://livinggoods.org/donate-2/']
 	],
 	[
 		['Code.org', 'https://code.org/donate'],
-		['Olin College Alumni Merit Scholarship Fund','https://fundit.olin.edu/project/555a1d3109206579b4677801']
+		['Olin College Alumni Merit Scholarship Fund','https://fundit.olin.edu/project/555a1d3109206579b4677801'],
+		['Powerful Vocies', 'http://www.powerfulvoices.org/']
 	],
 	[
 		['Olin College', 'https://www.olin.edu/invest/ways/making-your-gift-olin-college/'],
-		['Women&apos;s Medical Fund', 'http://wmfwisconsin.org/']
+		['Women&apos;s Medical Fund', 'http://wmfwisconsin.org/'],
+		['Lava Mae', 'http://www.lavamae.org/#!donate/cjiv']
 	],
 	[
-		['Ada Initiative', 'https://adainitiative.org/donate/']
+		['Ada Initiative', 'https://adainitiative.org/donate/'],
+		['charity: water', 'https://donate.charitywater.org/donate'],
+		['City Harvest', 'http://www.cityharvest.org/']
 	],
 	[
-		['App Camp for Girls', 'https://www.indiegogo.com/projects/app-camp-for-girls-3-0/x/3976761#/story']
+		['App Camp for Girls', 'https://www.indiegogo.com/projects/app-camp-for-girls-3-0/x/3976761#/story'],
+		['Girl Scouts of Western Washington', 'https://www.indiegogo.com/projects/girl-scouts-is-foreverygirl#/story']
 	],
 	[
 		['This American Life', 'http://www.thisamericanlife.org/donate'],
-		['Project VOICE', 'http://www.virgin.com/unite/donate/project-voice']
+		['Project VOICE', 'http://www.virgin.com/unite/donate/project-voice'],
+		['Risig Sun', 'http://www.risingsunkent.com/']
 	]
 ]
 
