eede7d077e991ef9b1e5427920ee88f9177c015d
Nikki Lee <nikki@nicoleblee.com>
2017-12-30 22:46:46 -0800

    Updated personal information (facts and job)

--- about.js
+++ about.js
@@ -6,12 +6,10 @@ var facts = [
 	"I got my black belt when I was 16.",
 	"I played the clarinet for more than 10 years.",
 	"I was 1 class away from a minor in Spanish (except that Olin doesn&apos;t have minors).",
-	"On a good day, I know all of the countries of the world.",
-	"My brother and I memorized the original 151 Pok&eacute;mon together.",
 	"I made One Velociraptor Per Child because OLPC employees told me it was hilarious.",
 	"I still have my wisdom teeth.",
 	"I was born on my due date.",
-	"My humanities capstone project one of most popular things I&apos;ve ever written.",
+	"My humanities capstone project is one of most popular things I&apos;ve ever written.",
 	"My wrists are too small to effectively break out of handcuffs.",
 	"It took me 216 tries to beat Spelunky.",
 	"A lot of people think I have a Canadian accent (I&apos;m from California).",
@@ -20,7 +18,11 @@ var facts = [
 	"I&apos;m a supertaster.",
 	"I was the first person to rate the book Supercollaborators on Goodreads.",
 	"I once tried to go to Rio de Janeiro and ended up in the Bahamas.",
-	"I can&apos;t play on off beats."
+	"I can&apos;t play on off beats.",
+	"I got status with a hotel chain without realizing it.",
+	"I got status with an airline without realizing it.",
+	"First-person video games make me motion sick. So do many over-the-shoulder games.",
+	"I'm good at magic eye puzzles."
 ];
 
 var NewFact = function() {
